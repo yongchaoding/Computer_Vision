9bcaf4b61b36dd1df2049c1ee634033eb075684e
Yongchao Ding <yongchaoding@outlook.com>
2017-12-11 14:29:24 +0800

    Modify Read_Data func and Add net train func
    1. Modify Read_Data function
    2. Add Net train function
    
    Signed-off-by: Yongchao Ding <yongchaoding@outlook.com>

---
+++ Kaggle/CIFAR-10/Net_Train.py
@@ -0,0 +1,350 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from mxnet import gluon
+from mxnet import image
+from Read_Data import ReadData
+from Read_Data import Image_Show
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+
+Path_Label = '../../../Data/'
+File_Name = 'Train_CIFAR10.csv'
+
+
+# In[2]:
+
+
+# LoadImageData = ReadData(File_Name, Path_Label)
+# print(LoadImageData.shape)
+
+
+# ### 此处根据batch_size进行数据的分组（包括测试与batch）
+#  此处根据实际需要（AlexNet的最小输入）需要对Image进行resize
+
+# In[3]:
+
+
+def loadTrainTestData(Data, batch_size, Train_ratio = 0.8, resize = None):
+    # 测试数据与训练数据的数量
+    Num_Train = int(len(Data) * Train_ratio)
+    Num_Test = len(Data) - Num_Train
+    print(Num_Train, Num_Test)
+    # 检测是否可被batch_size整除
+    if Num_Train%batch_size: # | Num_Test%batch_size:
+        print("batch_size is not suitable!")
+        return -1
+    Batch_Train = int(Num_Train/batch_size)
+    # 此处进行图像的resize
+    Image = []
+    if resize:
+        for i in range(len(Data)):
+            # print(Data[i,1:].shape)
+            # print(Data[i,1:].reshape(3, 32, 32).transpose(2,1,0).shape)
+            # print(misc.imresize(Data[i,1:].reshape(3, 32, 32), (resize,resize)))
+            Image.append(misc.imresize(Data[i,1:].reshape(3, 32, 32), (resize,resize)).flatten())
+            # Image.append(np.hstack((Data[i,:1], misc.imresize(Data[i,1:].reshape(3, 32, 32), (resize,resize)).flatten())))
+        Image = np.array(Image)
+        Image_Data = np.concatenate((Data[:,:1], Image), axis = 1).astype(np.uint8)
+        # 之前出现颜色问题是因为整型和浮点型的原因
+        plt.imshow(Image_Data[1,1:].reshape(resize, resize, 3))
+        plt.show()
+        Data = Image_Data;
+    # Batch_Test = int(Num_Test/batch_size)
+    # 将训练数据放入Batch_Train个batch中
+    Train_Data = []
+    for i in range(Batch_Train):
+        Train_Data.append(Data[i*batch_size:(i+1)*batch_size, :])
+    # 将剩下的数据作为Test_Data
+    Test_Data = Data[Batch_Train* batch_size: , :]
+    return np.array(Train_Data), np.array(Test_Data)
+
+
+# In[4]:
+
+
+# Data = np.ones((100, 3073))
+# print(Data.shape)
+# Train_Data, Test_Data = loadTrainTestData(LoadImageData, 256, resize=32)
+
+
+# In[5]:
+
+
+#plt.imshow(Image[2])
+#plt.show()
+# print(LoadImageData.shape)
+# print(Train_Data.shape)
+# print(Test_Data.shape)
+
+
+# In[6]:
+
+
+def GetImageAndLabel(Data, size = 32):
+    Label = Data[:,0]
+    Image = Data[:,1:].reshape((len(Data),size, size, 3)).transpose((0,3,2,1))
+    #print(Image.shape)
+    return Image/255, Label
+
+
+# In[7]:
+
+
+# Image, Label = GetImageAndLabel(Train_Data[0])
+# print(Image.shape)
+# print(Label.shape)
+
+
+# In[8]:
+
+
+def Image_show(Image, Label):
+    plt.imshow(Image.transpose(1,2,0))
+    plt.title(Label)
+    plt.show()
+
+
+# In[9]:
+
+
+# print(Image[0].shape)
+# Image_show(Image[0], Label[0])
+#image_resize = misc.imresize(Image[0], (224,224))
+#print(image_resize.shape)
+#Image_show(image_resize.transpose(2,1,0), Label[0])
+
+
+# ### 定义一个基础的神经网络
+
+# In[10]:
+
+
+from mxnet import gluon
+from mxnet import init
+from mxnet import nd
+from mxnet.gluon.model_zoo import vision
+from mxnet import autograd as ag
+from time import clock
+
+
+# AlexNet对输入的图形的像素尺寸有要求，不能用原先的32*32
+# AlexNet最后的全连接层需要根据实际需要，进行类别选择
+# AlexNet = vision.alexnet(classes=10)
+# #print(AlexNet)
+# AlexNet.initialize(init=init.Xavier())
+
+# loss = gluon.loss.SoftmaxCrossEntropyLoss()
+# Trainer = gluon.Trainer(ConvNet.collect_params(), 'sgd', {'learning_rate': 0.05})
+
+# x = nd.random_normal(shape=(10, 3, 224, 224))
+# print(x.shape)
+# y = AlexNet(x)
+# print(y.shape)
+
+from mxnet import gluon
+from mxnet import init
+from mxnet.gluon import nn
+# ConvNet = nn.Sequential()
+# with ConvNet.name_scope():
+#     ConvNet.add(
+#         nn.Conv2D(channels=20, kernel_size=5, activation='relu'),
+#         nn.MaxPool2D(pool_size=2, strides=2),
+#         nn.Conv2D(channels=50, kernel_size=3, activation='relu'),
+#         nn.MaxPool2D(pool_size=2, strides=2),
+#         nn.Flatten(),
+#         nn.Dense(128, activation="relu"),
+#         nn.Dense(10)
+#     )
+# ConvNet.initialize(init=init.Xavier())
+
+# loss = gluon.loss.SoftmaxCrossEntropyLoss()
+# Trainer = gluon.Trainer(ConvNet.collect_params(), 'sgd', {'learning_rate': 0.05})
+
+
+# In[11]:
+
+
+def accuracy(out, label):
+    return nd.mean(out.argmax(axis=1) == label).asscalar()
+
+
+# In[12]:
+
+
+def evaluate_accuracy(Data, net):
+    test_image, test_label = GetImageAndLabel(Data, size=32)
+    out = net(test_image)
+    # print(out.argmax(axis=1), " ", test_label)
+    acc = accuracy(out, test_label)
+    return acc
+
+
+# In[13]:
+
+
+def train(train_data, test_data, net, echoes, loss_func, trainer, size):
+    test_acc = 0
+    for echoe in range(echoes):
+        train_loss = 0
+        train_acc = 0
+        start_time = clock()
+        for train_batch in train_data:
+            #print(train_batch.shape)
+            batch_size = len(train_batch)
+            train_image, train_label = GetImageAndLabel(train_batch, size=size)
+            #print(train_image.shape)
+            #print(train_label.shape)
+            with ag.record():
+                #print(train_image[0])
+                out = net(train_image)
+                #print(out.shape)
+                #print(out.argmax(axis=1))
+                #print(train_label)
+                loss = loss_func(out, train_label)
+                #print(loss)
+            # 此处需要backward()！！！！
+            loss.backward()
+            # trainer.set_learning_rate(0.1)
+            trainer.step(batch_size)
+            train_loss += np.mean(loss.asnumpy())
+            # 局部正确率的计算
+            #print(train_label.shape)
+            #print(batch_size)
+            #print(out.argmax(axis=1))
+            #print(train_label)
+            #print(accuracy(out, train_label)/batch_size)
+            train_acc += accuracy(out, train_label)
+            # print(train_acc)
+        # 此处需要输出最后的loss及其精度
+        end_time = clock()
+        use_time = (end_time - start_time) #/1000000
+        test_acc = evaluate_accuracy(test_data, net)
+        print("Echoe is %d, Train Loss is %f, Train Acc is %f, Test Acc is %f, Use-time is %f S" % (echoe+1, train_loss/len(train_data), train_acc/len(train_data), test_acc, use_time))
+        
+        
+
+
+# In[14]:
+
+
+# train(nd.array(Train_Data), nd.array(Test_Data), ConvNet, 50, loss, Trainer, 32)
+
+
+# In[15]:
+
+
+# temp_out = nd.array([[-0.29596519,0.78109169,0.3583461,0.3933138,0.10683483,-0.18825993,-0.17625771,-0.33768478,-0.68717098,0.20490324],
+#                     [-0.29596519,0.78109169,0.8583461,0.3933138,0.10683483,-0.18825993,-0.17625771,-0.33768478,-0.68717098,0.20490324],
+#                     [-0.29596519,0.78109169,0.3583461,0.9933138,0.10683483,-0.18825993,-0.17625771,-0.33768478,-0.68717098,0.20490324]])
+# print(temp_out.shape)
+# print(temp_out.argmax(axis=1))
+
+
+# ## 调试网络
+
+# In[16]:
+
+
+# def softmax(X):
+#     exp = nd.exp(X)
+#     # 假设exp是矩阵，这里对行进行求和，并要求保留axis 1，
+#     # 就是返回 (nrows, 1) 形状的矩阵
+#     partition = exp.sum(axis=1, keepdims=True)
+#     return exp / partition
+# def cross_entropy(yhat, y):
+#     return - nd.pick(nd.log(yhat), y)
+
+
+# In[17]:
+
+
+# import sys
+# sys.path.append('../../../gluon-tutorials-zh')
+# from mxnet import ndarray as nd
+# from mxnet import autograd
+# import utils
+
+# batch_size = 256
+# train_data, test_data = utils.load_data_fashion_mnist(batch_size)
+
+
+# In[18]:
+
+
+# from mxnet import gluon
+# from mxnet import init
+# from mxnet.gluon import nn
+# ConvNet = nn.Sequential()
+# with ConvNet.name_scope():
+#     ConvNet.add(
+#         nn.Conv2D(channels=20, kernel_size=5, activation='relu'),
+#         nn.MaxPool2D(pool_size=2, strides=2),
+#         nn.Conv2D(channels=50, kernel_size=3, activation='relu'),
+#         nn.MaxPool2D(pool_size=2, strides=2),
+#         nn.Flatten(),
+#         nn.Dense(128, activation="relu"),
+#         nn.Dense(10)
+#     )
+# ConvNet.initialize(init=init.Xavier())
+
+
+# In[19]:
+
+
+# softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+# trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.005})
+
+
+# In[20]:
+
+
+# from mxnet import ndarray as nd
+# from mxnet import autograd
+
+# lr = 0.5
+
+# for epoch in range(100):
+#     test_acc = 0.
+#     train_loss = 0.
+#     train_acc = 0.
+#     #for data, label in train_data:
+#     for train_batch in Train_Data:
+#         data, label = GetImageAndLabel(train_batch, size=32)
+#         data = nd.array(data)
+#         #print(data.shape)
+#         label = nd.array(label)
+#         #print(label.shape)
+#         batch_size = len(data)
+#         with autograd.record():
+#             #print(data.shape)
+#             output = net(data)
+#             #temp_soft = softmax(output)
+#             #temp_loss = cross_entropy(temp_soft, label)
+#             #print(output)
+#             #print(output.argmax(axis=1))
+#             #print(label)
+#             #print(temp_soft)
+#             #print(temp_loss)
+            
+#             #print(label.shape)
+            
+#             # loss计算有问题
+#             loss = softmax_cross_entropy(output, label)
+#             #print(loss)
+#             #print(loss.shape)
+#         loss.backward()
+#         trainer.step(batch_size)
+        
+#         trainer.set_learning_rate(lr)
+#         train_loss += nd.mean(loss).asscalar()
+#         train_acc += accuracy(output, label)
+#     lr = lr * 0.9
+#     test_acc = evaluate_accuracy(nd.array(Test_Data), net)
+#     print("Epoch %d. Loss: %f, Train acc %f, Test acc %f, lr is %f" % (
+#         epoch, train_loss/len(Train_Data), train_acc/len(Train_Data), test_acc, lr))
+
--- Kaggle/CIFAR-10/Read_Data.py
+++ Kaggle/CIFAR-10/Read_Data.py
@@ -10,31 +10,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import preprocessing
 import os
-Path = '../../../Data/'
+# Path = '../../../Data/'
 # Data = pd.read_csv('CIFAR_10.csv')
 
 
 # In[ ]:
 
 
-Versobe_1 = True
+# Versobe_1 = True
 
-Image_Data = []
-Num_Files = 0
-for root, dirs, files in os.walk(Path+'train/'):
-    for each in files:
-        Num_Files += 1
-        img = mpimg.imread(Path+'train/'+each).transpose((2,1,0))
-        img = img.flatten()
-        img_id = np.array(int(each[:-4]))
-        Image_Data.append(np.hstack((img_id, img)))
-        if Versobe_1:
-            print(img_id)
-            print(img.shape)
-            print(each)
-            break
-ImageData = np.array(Image_Data)
-print(ImageData.shape)
+# Image_Data = []
+# Num_Files = 0
+# for root, dirs, files in os.walk(Path+'train/'):
+#     for each in files:
+#         Num_Files += 1
+#         img = mpimg.imread(Path+'train/'+each).transpose((2,1,0))
+#         img = img.flatten()
+#         img_id = np.array(int(each[:-4]))
+#         Image_Data.append(np.hstack((img_id, img)))
+#         if Versobe_1:
+#             print(img_id)
+#             print(img.shape)
+#             print(each)
+#             break
+# ImageData = np.array(Image_Data)
+# print(ImageData.shape)
 
 
 # In[2]:
@@ -49,37 +49,37 @@ def Image_Show(Data, Label_inverse):
     plt.title(Label_inverse[int(Data[0])])
     plt.show()
     
-# Image_Show(Data[15], Label_inverse)
+#Image_Show(Data[15], Label_inverse)
 #Image_Show(ImageData[0])
 
 
 # In[ ]:
 
 
-# 读入csv数据，此中包含图像信息及其Label信息
-# ImageData = np.loadtxt(open('CIFAR_10.csv','rb'), delimiter=',', skiprows=0)
+# # 读入csv数据，此中包含图像信息及其Label信息
+# # ImageData = np.loadtxt(open('CIFAR_10.csv','rb'), delimiter=',', skiprows=0)
 
-## 数组数据排列的测试程序
-x = np.array([[1,3], [2,3], [0,5]])
-print(x.shape)
-print(x)
-y = np.argsort(x[:,0])
-print(x[y])
+# ## 数组数据排列的测试程序
+# x = np.array([[1,3], [2,3], [0,5]])
+# print(x.shape)
+# print(x)
+# y = np.argsort(x[:,0])
+# print(x[y])
 
-print('Start sort')
-print(ImageData.shape)
-# 按array的第一列进行排序
-ImageData_arg = np.argsort(ImageData[:,0])
-ImageData = ImageData[ImageData_arg]
-# 测试与实际的第一幅图像是否一致
-Image_Show(ImageData[0])
-# print(ImageData[ImageData_arg])
+# print('Start sort')
+# print(ImageData.shape)
+# # 按array的第一列进行排序
+# ImageData_arg = np.argsort(ImageData[:,0])
+# ImageData = ImageData[ImageData_arg]
+# # 测试与实际的第一幅图像是否一致
+# Image_Show(ImageData[0])
+# # print(ImageData[ImageData_arg])
 
 
 # In[ ]:
 
 
-Image_Show(ImageData[2])
+# Image_Show(ImageData[2])
 
 
 # In[ ]:
@@ -92,7 +92,7 @@ Image_Show(ImageData[2])
 # ImageLabel = np.loadtxt(open(Path+'trainLabels.csv','rb'), delimiter=',', skiprows=0)
 
 # 由于存在标志，因此需要借助padans来进行csv文件的读取
-ImageLabel = pd.read_csv(Path+'trainLabels.csv')
+# ImageLabel = pd.read_csv(Path+'trainLabels.csv')
 
 
 # ### LabelEncoder 与 One-Hot 功能测试
@@ -100,35 +100,35 @@ ImageLabel = pd.read_csv(Path+'trainLabels.csv')
 # In[ ]:
 
 
-# print(ImageLabel)
-label_id = np.array(ImageLabel['id'])
-label = np.array(ImageLabel['label'])
-print(label)
+# # print(ImageLabel)
+# label_id = np.array(ImageLabel['id'])
+# label = np.array(ImageLabel['label'])
+# print(label)
 
-# 将描述型数据变换为数字表示
-encoder = preprocessing.LabelEncoder()
-interger_label = encoder.fit_transform(label).reshape(-1, 1)
-print(interger_label)
-one_hot = preprocessing.OneHotEncoder(sparse=False)
-data = one_hot.fit_transform(interger_label)
-print(data)
+# # 将描述型数据变换为数字表示
+# encoder = preprocessing.LabelEncoder()
+# interger_label = encoder.fit_transform(label).reshape(-1, 1)
+# print(interger_label)
+# one_hot = preprocessing.OneHotEncoder(sparse=False)
+# data = one_hot.fit_transform(interger_label)
+# print(data)
 
 
 # In[ ]:
 
 
-data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
-values = np.array(data)
-print(values)
-# integer encode
-label_encoder = preprocessing.LabelEncoder()
-integer_encoded = label_encoder.fit_transform(values).reshape(len(integer_encoded), 1)
-print(integer_encoded)
-# binary encode
-onehot_encoder = preprocessing.OneHotEncoder(sparse=False)
-# integer_encoded = integer_encoded
-onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-print(onehot_encoded)
+# data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+# values = np.array(data)
+# print(values)
+# # integer encode
+# label_encoder = preprocessing.LabelEncoder()
+# integer_encoded = label_encoder.fit_transform(values).reshape(len(integer_encoded), 1)
+# print(integer_encoded)
+# # binary encode
+# onehot_encoder = preprocessing.OneHotEncoder(sparse=False)
+# # integer_encoded = integer_encoded
+# onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+# print(onehot_encoded)
 
 
 # ### 此处进行数组的拼接
@@ -136,37 +136,37 @@ print(onehot_encoded)
 # In[ ]:
 
 
-print(label.shape)
-print(ImageData.shape)
+# print(label.shape)
+# print(ImageData.shape)
 
 
 # In[ ]:
 
 
-# 简易二维数组拼接处理
-x = np.array([[1,2],[3,4],[5,6]])
-y = np.array([1, 2, 3])
-print(x.shape)
-print(y.shape)
-# 二维数组切片测试
-z = x[:,1:]
-c = np.concatenate((y.reshape(1, -1).T, x), axis = 1)
-print(c)
-print(z)
-np.random.shuffle(c)
-print(c)
+# # 简易二维数组拼接处理
+# x = np.array([[1,2],[3,4],[5,6]])
+# y = np.array([1, 2, 3])
+# print(x.shape)
+# print(y.shape)
+# # 二维数组切片测试
+# z = x[:,1:]
+# c = np.concatenate((y.reshape(1, -1).T, x), axis = 1)
+# print(c)
+# print(z)
+# np.random.shuffle(c)
+# print(c)
 
 
 # In[ ]:
 
 
-ImageData_1 = np.zeros((50, 3073))
-print(ImageData_1.shape)
-ImageData_1 = ImageData_1[:,1:]
-print(ImageData_1.shape)
-Train_CIFAR = np.concatenate((label.reshape(1, -1).T, ImageData), axis =1)
-print(Train_CIFAR.shape)
-print(Train_CIFAR)
+# ImageData_1 = np.zeros((50, 3073))
+# print(ImageData_1.shape)
+# ImageData_1 = ImageData_1[:,1:]
+# print(ImageData_1.shape)
+# Train_CIFAR = np.concatenate((label.reshape(1, -1).T, ImageData), axis =1)
+# print(Train_CIFAR.shape)
+# print(Train_CIFAR)
 
 
 # ## 下面是基于上述测试的，对测试数据的读取及其保存
@@ -254,13 +254,13 @@ def ReadData(file_name, path):
 # In[8]:
 
 
-Data,Label_inverse  = MixImageAndLabel(Path_Image, Path_Label)
+# Data,Label_inverse  = MixImageAndLabel(Path_Image, Path_Label)
 
 
 # In[13]:
 
 
-SaveData2CSV(Data, File_Name, Path_Label)
+# SaveData2CSV(Data, File_Name, Path_Label)
 
 
 # In[ ]:
@@ -274,26 +274,26 @@ SaveData2CSV(Data, File_Name, Path_Label)
 # In[ ]:
 
 
-Data = ImageDataLoad(Path_Image)
+# Data = ImageDataLoad(Path_Image)
 
 
 # In[ ]:
 
 
-print(Data.shape)
-print(Data[1])
+# print(Data.shape)
+# print(Data[1])
 
 
 # In[ ]:
 
 
-ImageData_arg = np.argsort(Data[:,0])
-ImageData = Data[ImageData_arg]
+# ImageData_arg = np.argsort(Data[:,0])
+# ImageData = Data[ImageData_arg]
 
 
 # In[12]:
 
 
 # print(ImageData[0])
-Image_Show(Data[2], Label_inverse)
+# Image_Show(Data[2], Label_inverse)
 
